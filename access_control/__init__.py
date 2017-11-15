847b460d977d0a4ffd3fa8ac58bae3dccc42e498
Victor E <victorevector@gmail.com>
2017-11-15 15:10:50 -0800

    fixed formattg

--- access_control/__init__.py
+++ access_control/__init__.py
@@ -2,70 +2,19 @@
 from __future__ import absolute_import
 import subprocess
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class Access_controlPlugin(octoprint.plugin.SettingsPlugin,
                            octoprint.plugin.AssetPlugin,
                            octoprint.plugin.TemplatePlugin,
                            octoprint.plugin.StartupPlugin):
-
     def on_startup(self):
         self.global_set(['accessControl','enabled'], 'true')
         self.global_set(['server','firstRun'], 'true')
         s = subprocess.call(['/home/pi/oprint/bin/pip', 'uninstall', '-y', 'access_control'])
         subprocess.call(['sudo', 'reboot'])
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/access_control.js"],
-			css=["css/access_control.css"],
-			less=["less/access_control.less"]
-		)
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			access_control=dict(
-				displayName="Access_control Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="victorevector",
-				repo="access_control",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/victorevector/access_control/archive/{target_version}.zip"
-			)
-		)
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Access_control Plugin"
 
 def __plugin_load__():
@@ -74,5 +23,4 @@ def __plugin_load__():
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 	}
